ffa89b201595c9bce4d262f0ae96224fc7734b1d
Syed Mehdi <syed@bcw.group>
2024-09-08 22:09:33 +0500

    deployment lock

--- run.sh
+++ run.sh
@@ -22,14 +22,14 @@ RPC=${RPC_URL} MNEMONIC=${MNEMONIC} npm run submit --prefix /app/prereq/safe-sin
 
 # This created a `deployment.json` file in the `/app/prereq/safe-singleton-factory/artifacts/${CHAIN_ID}` directory
 SINGLETON_FACTORY="/app/prereq/safe-singleton-factory/artifacts/${CHAIN_ID}"
-# Copy the singleton factory to safe-smart-contracts directory named `singleton-factory-deployments`
-mkdir -p /app/prereq/safe-smart-contracts/singleton-factory-deployments
-cp -r $SINGLETON_FACTORY /app/prereq/safe-smart-contracts/singleton-factory-deployments
-ls /app/prereq/safe-smart-contracts/singleton-factory-deployments
+# Copy the singleton factory to safe-smart-account directory named `singleton-factory-deployments`
+mkdir -p /app/prereq/safe-smart-account/singleton-factory-deployments
+cp -r $SINGLETON_FACTORY /app/prereq/safe-smart-account/singleton-factory-deployments
+ls /app/prereq/safe-smart-account/singleton-factory-deployments
 
-cat /app/prereq/safe-smart-contracts/singleton-factory-deployments/${CHAIN_ID}/deployment.json
+cat /app/prereq/safe-smart-account/singleton-factory-deployments/${CHAIN_ID}/deployment.json
 
-cd /app/prereq/safe-smart-contracts
+cd /app/prereq/safe-smart-account/
 MNEMONIC=${MNEMONIC} yarn hh deploy --network customnetwork
 MNEMONIC=${MNEMONIC} yarn hh deploy-proxy --network customnetwork
 MNEMONIC=${MNEMONIC} yarn hh deploy-test-erc20 --network customnetwork
