340874db061dab2ffbedafe5a28a586178a180f8
StephenMal <Maldonado527@gmail.com>
2025-01-23 15:55:10 +0400

    added lock and removed idx

--- merkle-store-hardhat/contracts/UpgradableMerkleStoreV2.sol
+++ merkle-store-hardhat/contracts/UpgradableMerkleStoreV2.sol
@@ -16,8 +16,8 @@ contract UpgradableMerkleStoreV2 is Initializable, AccessControlUpgradeable, UUP
     bytes32 public constant OPERATIONAL_ADMIN_ROLE = keccak256("OPERATIONAL_ADMIN_ROLE");
 
     // Events 
-    event NewRootSubmission(bytes32 indexed idx, bytes32 indexed merkleRoot, string data, string metadata);
-    event RootMetadataModification(bytes32 indexed idx, string metadata);
+    event NewRootSubmission(bytes32 indexed merkleRoot, bool indexed lock, string data, string metadata);
+    event RootMetadataModification(bytes32 indexed merkleRoot, bool indexed lock, string metadata);
 
     /// A Merkle Tree submission
     struct Submission {
@@ -25,14 +25,15 @@ contract UpgradableMerkleStoreV2 is Initializable, AccessControlUpgradeable, UUP
         bytes32 merkleRoot;
         /// Immutable data, should be used for any data that needs to be stored permanently
         /// with this tree.
-        string data;
+        string metadata;
+        /// Lock prevents modifying the submission.
+        bool lock;
         /// Mutable metadata, should be used for things that can change depending on
         /// how it runs.
-        string metadata;
-        /// The block number of the Submission
         uint256 submittedOn;
         /// The block number of the UpdatedOn
         uint256 updatedOn;
+
     }
 
     /// @notice Initializes the contract and sets up roles
@@ -62,11 +63,14 @@ contract UpgradableMerkleStoreV2 is Initializable, AccessControlUpgradeable, UUP
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADE_ADMIN_ROLE) {}
 
     /// @notice Submit a new Merkle root with metadata
+    /// @param merkleRoot The merkle root of the tree submission
+    /// @param metadata Any metadata stored 
+    /// @param lock Whether or not the submission data is locked.
+    /// @return hashedMerkleRoot Returns the hashed merkle root
     function submitNewMerkleRootWithMetadata(
-        bytes32 idx,
         bytes32 merkleRoot,
-        string calldata data,
-        string calldata metadata
+        string calldata metadata,
+        bool lock
     ) external onlyRole(OPERATIONAL_ADMIN_ROLE) returns (bytes32) {
         // Increment root nonce
         uint256 rootNonce = StorageSlot.getUint256Slot(_ROOT_NONCE_SLOT).value + 1;
@@ -74,47 +78,77 @@ contract UpgradableMerkleStoreV2 is Initializable, AccessControlUpgradeable, UUP
 
         // Store new submission
         mapping(bytes32 => Submission) storage merkleRoots = _getMerkleRootsStorage();
-        merkleRoots[idx] = Submission(
+        merkleRoots[merkleRoot] = Submission(
             merkleRoot,
-            data,
             metadata,
+            lock,
             block.number,
             block.number
         );
 
-        emit NewRootSubmission(idx, merkleRoot, data, metadata);
+        emit NewRootSubmission(merkleRoot, lock, metadata, metadata);
         return sha256(abi.encodePacked(merkleRoot));
     }
 
-    /// @notice Update metadata for an existing Merkle root
+    /// @notice Update metadata & lock for an existing Merkle root
+    /// @param merkleRoot The merkle root of the tree we look up
+    /// @param newMetadata Additional data (JSON?) stored with the tree.
+    /// @param lock Whether or not the submission should be locked, cannot upgrade after that.
+    /// @return submission Returns the updated submission
     function updateMetadataOnMerkleRootHash(
-        bytes32 idx,
-        string calldata newMetadata
+        bytes32 merkleRoot,
+        string calldata newMetadata,
+        bool lock
     ) external onlyRole(OPERATIONAL_ADMIN_ROLE) returns (Submission memory) {
         mapping(bytes32 => Submission) storage merkleRoots = _getMerkleRootsStorage();
-        Submission storage prevSubmission = merkleRoots[idx];
+        Submission storage prevSubmission = merkleRoots[merkleRoot];
 
+        require(!prevSubmission.lock, "Submission is locked");
         require(prevSubmission.submittedOn != 0, "Submission does not exist");
 
         prevSubmission.metadata = newMetadata;
+        prevSubmission.lock = lock;
         prevSubmission.updatedOn = block.number;
 
-        emit RootMetadataModification(idx, newMetadata);
+        emit RootMetadataModification(merkleRoot, lock, newMetadata);
+        return prevSubmission;
+    }
+
+    /// @notice Update lock on the data in the hash
+    /// @param merkleRoot The merkle root of the tree we look up to lock
+    /// @return Submission Returns the submission in its final, locked state.
+    function lockMetadataOnMerkleRootHash(
+        bytes32 merkleRoot
+    ) external onlyRole(OPERATIONAL_ADMIN_ROLE) returns (Submission memory) {
+        mapping(bytes32 => Submission) storage merkleRoots = _getMerkleRootsStorage();
+        Submission storage prevSubmission = merkleRoots[merkleRoot];
+
+        require(!prevSubmission.lock, "Submission was already locked");
+        require(prevSubmission.submittedOn != 0, "Submission does not exist");
+
+        prevSubmission.lock = true;
+        prevSubmission.updatedOn = block.number;
+
+        emit RootMetadataModification(merkleRoot, true, prevSubmission.metadata);
         return prevSubmission;
     }
 
     /// @notice Get submission details by index
-    function getMerkleRootOnHash(bytes32 idx) external view returns (Submission memory) {
+    /// @param merkleRoot Returns the Submission of a merkle tree
+    /// @return Submission The submission for a given merkleRoot
+    function getMerkleRootOnHash(bytes32 merkleRoot) external view returns (Submission memory) {
         mapping(bytes32 => Submission) storage merkleRoots = _getMerkleRootsStorage();
-        return merkleRoots[idx];
+        return merkleRoots[merkleRoot];
     }
 
     /// @notice Get the total number of Merkle roots submitted
+    /// @return numberMerkleRoots Returns the number of Merkle Roots stored in the contract
     function getNumberOfMerkleRoots() external view returns (uint256) {
         return StorageSlot.getUint256Slot(_ROOT_NONCE_SLOT).value;
     }
 
     /// @dev Internal function to access the merkle roots storage
+    /// @return merkleRoots Returns a mapping of a merkleRoot to submission data about the merkle tree
     function _getMerkleRootsStorage() private view returns (mapping(bytes32 => Submission) storage merkleRoots) {
         bytes32 slot = _MERKLE_ROOTS_SLOT;
         assembly {
